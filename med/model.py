35bb8460482b96ee1b549ef8af893ad77d235af2
satyam9k <75788300+satyam9k@users.noreply.github.com>
2024-03-25 06:21:14 +0530

    Update model.py

--- med/model.py
+++ med/model.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/Clinical-AI-Apollo/Medical-NER"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer your_token"}
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
