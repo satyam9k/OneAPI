d7fd0d76ecb27b27eef1bc62c7ec8bba86331568
satyam9k <75788300+satyam9k@users.noreply.github.com>
2024-03-25 06:21:49 +0530

    Update output.py

--- med/output.py
+++ med/output.py
@@ -2,18 +2,14 @@
 from tabulate import tabulate
 
 def generate_table_and_summary(entities):
-    # Check if entities is a string
     if isinstance(entities, str):
         print("Error: Expected list of dictionaries, received a string.")
         return None, None
     
     # Convert output to a list of lists for tabulate
     table_data = [[entity['entity_group'],  entity['word']] for entity in entities]
-    
-    # Define headers for the table
     headers = ["Entity Group",  "Word"]
 
-    # Print the table
     print("Structured Table:")
     print(tabulate(table_data, headers=headers))
 
